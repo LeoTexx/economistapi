01645ab0b2fb6507ddb586f325d2775d6fefd3df
LeoTexx <LeoTexx@users.noreply.github.com>
2021-06-01 22:10:15 +0100

    auth

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
 const express = require("express");
-const cors = require("cors");
+const bodyParser = require("body-parser");
+const InitiateMongoServer = require("./config/db");
+const user = require("./routes/user");
 const { spawn } = require("child_process");
+
+InitiateMongoServer();
+
 const app = express();
 const PORT = process.env.PORT || 3333;
-app.use(cors());
+
+app.use(bodyParser.json());
+
+app.use("/user", user);
+
 app.get("/", (req, res) => {
   var dataToSend;
   // spawn new child process to call the python script
@@ -17,11 +26,9 @@ app.get("/", (req, res) => {
   python.on("close", (code) => {
     console.log(`child process close all stdio with code ${code}`);
     // send data to browser
+
     res.send(dataToSend);
   });
-});
-app.post("/data", (req, res) => {
-  res.json(dataToSend);
 });
 app.listen(PORT, () => {
   console.warn(`App listening on http://localhost:${PORT}`);
